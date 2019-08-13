6ceff46793908e59ec91e1154cc240346784b800
IcyW <hbj032215@yeah.net>
2019-08-13 20:33:01 +0800

    +)Fizz 第一个场景已处理

---
+++ practice/fizzbuzz.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+"""
+@Time    : 13/8/2019 下午8:05
+@Author  : Icy Huang
+@Site    : 
+@File    : fizzbuzz.py
+@Software: PyCharm Community Edition
+@Python  : 
+"""
+
+
+class FizzBuzz:
+    def __init__(self, m, n):
+        self.m = m
+        self.n = n
+        self.x = 1
+
+    def say(self, x):
+        self.x = x
+        if self.x % self.m == 0:
+            return "Fizz"
